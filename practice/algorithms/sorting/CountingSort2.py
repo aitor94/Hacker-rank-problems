3555d9d650c1e851b20d501ef2a011a2d4f82557
aitor <martinredondoaitor@gmail.com>
2018-05-01 21:21:46 +0200

    Solved Counting Sort 2

---
+++ practice/algorithms/sorting/CountingSort2.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+def countingSort(arr):
+    return sorted(arr)
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    result = countingSort(arr)
+    print (" ".join(map(str, result)))
+
